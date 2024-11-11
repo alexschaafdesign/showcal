7fddde1b9c30c8973af564e772a087189d4f1687
Alex Schaaf <musicdaddy@Alexs-MacBook-Pro-2.local>
2024-11-11 16:03:58 -0600

    Save current progress before trying reinstall of fullcalendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calendar from './pages/Calendar.js';  // Adjust the path if necessary
 import Home from './pages/Home.js';  // Example for a homepage, adjust as needed
+import './styles/App.css';  // Global styles for the app
+
 
 function App() {
   return (
